6db02e66ec1bfb96919982bcb31d64a18a8d2d36
ZiganshinDamir <49669653+ZiganshinDamir@users.noreply.github.com>
2019-04-20 11:34:23 +0300

    Add files via upload
    Напишите программу, которая считывает целое число и выводит текст, аналогичный приведенному в примере (важно в точности соблюдать вывод программы: обратите внимание на пробелы и на точки). Нельзя пользоваться конкатенацией строк, используйте print с несколькими параметрами.

---
+++ EX.1.py
@@ -0,0 +1,5 @@
+N = int(input())
+e1 = N + 1
+e2 = N - 1
+print('The next number for the number ' + str(N) + ' is ' + str(e1) + '.')
+print('The previous number for the number ' + str(N) + ' is ' + str(e2) + '.')
